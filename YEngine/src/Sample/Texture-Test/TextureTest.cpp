8cd0c46ba8703faa2ee929246ce3003766e507ea
wq.hero_bbq <chen.zike@immomo.com>
2019-12-12 18:45:34 +0800

    第二个例子

---
+++ YEngine/src/Sample/Texture-Test/TextureTest.cpp
@@ -0,0 +1,42 @@
+#include "TextureTest.h"
+#include <glad/glad.h> 
+#include <glfw3.h>
+#include "YRenderer.h"
+#include "YLoadShader.h"
+
+
+TextureTest::TextureTest()
+	:m_shader(nullptr)
+{
+
+}
+
+TextureTest::~TextureTest()
+{
+
+}
+TextureTest* TextureTest::create()
+{
+
+}
+
+void TextureTest::draw(YRenderer* renderer, const glm::mat4& transform, uint32_t flags)
+{
+
+}
+
+bool TextureTest::init()
+{
+
+}
+
+void TextureTest::initBuffers()
+{
+
+}
+
+void TextureTest::onDraw(const glm::mat4* transform, uint32_t)
+{
+
+}
+
